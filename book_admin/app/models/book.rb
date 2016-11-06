f06c7b4725f3cf6289533725afeb53057422bdd2
seiya kawamura <seiya.kawamura@gmail.com>
2016-11-06 23:25:51 +0900

    init book app

---
+++ book_admin/app/models/book.rb
@@ -0,0 +1,19 @@
+class Book < ActiveRecord::Base
+  default_scope -> { order('published_on desc') }
+  scope :costly, -> { where('price > ?', 3000) }
+  scope :written_about, -> (theme) { where('name like ?', "%#{theme}%") }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :price, numericality: { greater_than_or_equel_to: 0 }
+
+  validate do |book|
+    if book.name.include?('exercise')
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+end
---
+++ book_admin/test/models/auhor_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AuhorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
